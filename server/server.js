a6a32f115eb2938443d8206db57264a9ce05740d
Michael Crockett <mike.crockett76@gmail.com>
2019-10-23 12:06:47 +0100

    server mongo communication

--- server/server.js
+++ server/server.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, function(){
+const parser = require('body-parser');
+const cors = require('cors');
+
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router.js');
+
+app.use(cors());
+app.use(parser.json());
+
+
+MongoClient.connect('mongodb://localhost:27017')
+  .then((client) => {
+  const db = client.db('hotel');
+  const guestsCollection = db.collection('guests');
+  const guestsRouter = createRouter(guestsCollection);
+  app.use('/api/guests', guestsRouter);
+})
+.catch(console.err);
+
+app.listen(3000, function() {
 console.log("App is running on port 3000");
 });
